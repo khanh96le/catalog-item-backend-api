037db373e57b7a21b25a930482883c0d85d9a79a
jerry <khanh96le@gmail.com>
2018-10-22 15:45:22 +0700

    feat(article serializer): add article's validation for text length of title and body

--- main/serializers/article.py
+++ main/serializers/article.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
-from marshmallow import fields, pre_load, post_dump
+from marshmallow import fields, pre_load, post_dump, validate
 
 from main.serializers.base import BaseSchema
 from main.serializers.user import UserSchema
 
 
 class ArticleSchema(BaseSchema):
-    title = fields.String(required=True)
+    title = fields.String(required=True,
+                          validate=validate.Length(min=4, max=250))
     description = fields.String()
     slug = fields.String()
-    body = fields.String(required=True)
+    body = fields.String(required=True, validate=validate.Length(min=40))
 
     id = fields.Integer(dump_only=True)
     created_at = fields.DateTime(dump_only=True, required=True)
